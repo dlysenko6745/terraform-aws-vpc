c3bd66f4cd34bc5af27a1419f3fdd68fe0a2df35
dlysenko6745 <161907470+dlysenko6745@users.noreply.github.com>
2024-05-06 20:46:49 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,18 @@
+terraform {
+  required_version = ">= 1.0.0"
+}
+
+# Include the VPC module
+module "my_vpc" {
+  source = "./modules/vpc"
+  
+  # Pass variables to the module
+  vpc_cidr_block = var.vpc_cidr_block
+}
+
+# Output the VPC ID for reference
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = module.my_vpc.vpc_id
+}
 
