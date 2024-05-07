bcf80a493b4e6af6dfe14aca76d258d42e245c48
dlysenko6745 <161907470+dlysenko6745@users.noreply.github.com>
2024-05-06 20:44:26 -0500

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,1 +1,26 @@
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr_block
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "${cidrsubnet(var.vpc_cidr_block, 8, 0)}"
+  availability_zone = "us-east-2a" 
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public"
+  }
+}
+
+resource "aws_subnet" "private" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "${cidrsubnet(var.vpc_cidr_block, 8, 1)}"
+  availability_zone = "us-east-2b" 
+  tags = {
+    Name = "private"
+  }
+}
 
