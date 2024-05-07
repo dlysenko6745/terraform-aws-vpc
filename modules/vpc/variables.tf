d5aaf77ba98601f469ba3475b819b420907b81d0
dlysenko6745 <161907470+dlysenko6745@users.noreply.github.com>
2024-05-06 20:39:19 -0500

    Update variables.tf

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,1 +1,12 @@
+resource "aws_vpc" "terraform" {
+  cidr_block = "10.0.0.0/16"
+}
 
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.terraform.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "terraform"
+  }
+}
